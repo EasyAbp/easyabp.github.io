b2c9eb6510b468e8ab0dbd9b7131ce548fd9d014
croatiaLu <1369248927@qq.com>
2020-08-14 14:57:15 +0800

    upd: update vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,10 @@
 module.exports = {
-  base: "/easyabp.io/",
-  title: "EasyAbp", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-  description: "", // meta 中的描述文字，用于SEO
-  //注入到当前页面的 HTML <head> 中的标签
+  title: "EasyAbp",
+  description: "",
   head: [["link", { rel: "icon", href: "/icon.png" }]],
   themeConfig: {
-    logo: "/icon.png", //网页顶端导航栏左上角的图标
+    logo: "/icon.png",
     //   顶部导航栏
-    nav: [
-      // 格式二：添加下拉菜单，link指向的文件路径
-      { text: "Github", link: "https://github.com/EasyAbp" },
-    ],
-    // 侧边栏导航：会根据当前的文件路径是否匹配侧边栏数据，自动显示/隐藏
-    sidebar: {},
+    nav: [{ text: "Github", link: "https://github.com/EasyAbp" }],
   },
 };
